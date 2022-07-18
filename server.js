2b90b26cd3075ae2cef2957bf8216fd73147f971
elenareach@gmail.com <elenareach@gmail.com>
2022-07-18 12:34:29 -0600

    Node-react app deploy to heroku

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, '/trydo/build')));
+//app.use(express.static(path.join(__dirname, '/react-app/build')));
+
+
+app.get("/*", (req,res)=>{
+    res.sendFile(__dirname+"/trydo/build/index.html")     
+})
+
+// app.get("/react", (req,res)=>{
+//     res.sendFile(__dirname+"/react-app/build/index.html")
+// })
+
+app.get("/trydo", (req,res)=>{
+    res.sendFile(__dirname+"/html/index.html")
+})
+
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
